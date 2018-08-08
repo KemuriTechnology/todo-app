3e7148d8893665dad6c51141e42ccb3cc34b0a27
akshay <akshay@kemuri.in>
2018-08-08 15:40:18 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+import todo from './components/todo';
+
+export default {
+  name: 'App',
+  components: {
+    todo
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/utils/localstorage.js
@@ -0,0 +1,40 @@
+const STORAGE_KEY = 'todos-vuejs-2.0';
+
+export function fetch () {
+  var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+  todos.forEach(function (todo, index) {
+    todo.id = index;
+  });
+  todos.uid = todos.length;
+  return todos;
+}
+
+export function save (todos) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+}
+
+// visibility filters
+
+export function all (todos) {
+  return todos;
+}
+
+export function active (todos) {
+  return todos.filter(function (todo) {
+    return !todo.completed;
+  });
+}
+
+export function completed (todos) {
+  return todos.filter(function (todo) {
+    return todo.completed;
+  });
+}
+
+export default {
+  fetch,
+  save,
+  all,
+  active,
+  completed
+};
